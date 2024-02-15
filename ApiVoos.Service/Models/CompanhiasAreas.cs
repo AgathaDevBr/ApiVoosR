970adcbdb711eb8b28eda76a8bd8749be5e293dc
Ágatha Almeida <142939923+AgathaDevBr@users.noreply.github.com>
2024-02-15 16:51:23 -0300

    Update CompanhiasAreas.cs

--- ApiVoos.Service/Models/CompanhiasAreas.cs
+++ ApiVoos.Service/Models/CompanhiasAreas.cs
@@ -1,14 +1,28 @@
-﻿namespace ApiVoos.Service.Models
+using Newtonsoft.Json;
+
+namespace ApiVoos.Service.Models
 {
     public class CompanhiasAreas
     {
-
+        [JsonProperty("FlightNumber")]
         public string? voo {  get; set; }
+
+        [JsonProperty("Carrier")]
         public string? companhia { get; set; }
+
+        [JsonProperty("OriginAirport")]
         public string? origem { get; set; }
+
+        [JsonProperty("DestinationAirport")]
         public string? destino { get; set; }
+
+        [JsonProperty("DepartureDate")]
         public DateTime? partida { get; set; }
+
+        [JsonProperty("ArrivalDate")]
         public DateTime? chegada { get; set; }
+
+        [JsonProperty("FarePrice")]
         public double? tarifa { get; set; }
     }
 }
